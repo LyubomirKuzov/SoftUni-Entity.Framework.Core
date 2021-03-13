c3f7c168169e88b13a775873c61eed8a95298aa3
LyubomirKuzov <lyubomir.kuzov.99@gmail.com>
2021-03-13 21:39:27 +0200

    Add

---
+++ 08. C# Auto Mapping Objects/FastFood.Data/FastFoodContextDesignTimeFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FastFood.Data
+{
+    public class FastFoodContextDesignTimeFactory : IDesignTimeDbContextFactory<FastFoodContext>
+    {
+        public FastFoodContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<FastFoodContext>();
+
+            builder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=FastFood;Trusted_Connection=True;MultipleActiveResultSets=true;Integrated Security=true");
+
+            return new FastFoodContext(builder.Options);
+        }
+    }
+}
